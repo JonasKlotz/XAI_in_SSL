781ea494f701b45f276f4a005daf47a55c1e4f4c
Jonas Klotz <jonas.klotz@knime.com>
2023-07-03 13:07:02 +0200

    Use correct CIFAR transformation

--- src/datasets/cifar10.py
+++ src/datasets/cifar10.py
@@ -1,14 +1,18 @@
+import torch
 from pl_bolts.datamodules import CIFAR10DataModule
-from torchvision import transforms
+from torchvision import transforms as transforms_lib
+from pl_bolts.transforms.dataset_normalizations import cifar10_normalization
 
 
-def load_cifar10_data_module(data_dir="/home/jonasklotz/Studys/23SOSE/XAI_in_SSL/data/cifar10", batch_size=32):
-    cifar10_dm = CIFAR10DataModule(data_dir=data_dir, batch_size=batch_size)
-    transformations = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean=[x / 255.0 for x in [125.3, 123.0, 113.9]],
-            std=[x / 255.0 for x in [63.0, 62.1, 66.7]]
-        )
-    ])
-    return cifar10_dm, transformations
+def load_cifar10_data_module(data_dir="/home/jonasklotz/Studys/23SOSE/XAI_in_SSL/data/cifar10", batch_size=32, resize=None):
+    cf10_transforms = [transforms_lib.ToTensor(), cifar10_normalization()]
+    if resize:
+        cf10_transforms += [transforms_lib.Resize(resize, antialias=True)]
+    cf10_transforms = transforms_lib.Compose(cf10_transforms)
+
+    cifar10_dm = CIFAR10DataModule(data_dir=data_dir, batch_size=batch_size, normalize=True)
+
+    mean = torch.tensor(cifar10_dm.default_transforms().transforms[1].mean)
+    std = torch.tensor(cifar10_dm.default_transforms().transforms[1].std)
+    unnormalize = transforms_lib.Normalize((-mean / std).tolist(), (1.0 / std).tolist())
+    return cifar10_dm, unnormalize
